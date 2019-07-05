303ed38fed9ac05e56b68899591320fcb6531f45
kittaka <nra06554@nifty.com>
2019-07-06 00:17:32 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+﻿N,M=map(int,input().split())
+arr = [[0 for i in range(2)] for j in range(N)]
+goukei=0
+count=0
+for i in range(N):
+    arr[i][0],arr[i][1]=map(int,input().split())
+    
+    
+k=0    
+arr.sort()
+while M-count>0:
+    goukei+=arr[k][0]
+    arr[k][1]-=1
+    
+    if arr[k][1]==0:
+        k+=1
+    
+    count+=1
+ 
+print(goukei)
